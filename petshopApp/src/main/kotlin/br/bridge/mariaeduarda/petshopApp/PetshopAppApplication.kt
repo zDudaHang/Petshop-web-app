736f930c980d4838ec03d6e011bc6b701518dd35
zDudaHang <maria.eduarda.melo.hang@gmail.com>
2020-10-21 11:16:47 -0300

    First Commit

---
+++ petshopApp/src/main/kotlin/br/bridge/mariaeduarda/petshopApp/PetshopAppApplication.kt
@@ -0,0 +1,11 @@
+package br.bridge.mariaeduarda.petshopApp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PetshopAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<PetshopAppApplication>(*args)
+}
---
+++ petshopApp/src/test/kotlin/br/bridge/mariaeduarda/petshopApp/PetshopAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.bridge.mariaeduarda.petshopApp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PetshopAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
